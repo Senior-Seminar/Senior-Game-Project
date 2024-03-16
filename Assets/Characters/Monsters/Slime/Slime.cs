360ff1e969ab40b1b86481d36285a3494c6735ba
Andrew Lin <lin.andrew797@gmail.com>
2024-03-16 18:37:11 -0400

    Revert "Fix flipping animation on x axis accordingly without changing scale"
    This reverts commit 39bfd2b9b634eb71bebbc41cd95ab4a75a351585.

--- Assets/Characters/Monsters/Slime/Slime.cs
+++ Assets/Characters/Monsters/Slime/Slime.cs
@@ -3,9 +3,123 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
-public class Slime2 : MonoBehaviour
+public class Slime2 : MonoBehaviour, IDamageable
 {
+    Animator animator;
+    Rigidbody2D rb;
+    Collider2D physicsCollider;
+
     public float damage = 1;
+
+    bool isAlive = true;
+
+    private Transform playerTransform;
+
+    //health property
+    public float Health
+    {
+        set
+        {
+            if (value < _health)
+            {
+                animator.SetTrigger("hit");
+            }
+
+            _health = value;
+
+            //trigger death when health is equal or below 0
+            if (_health <= 0)
+            {
+                animator.SetBool("isAlive", false);
+                Targetable = false;
+            }
+
+        }
+        get
+        {
+            return _health;
+        }
+    }
+
+    public bool Targetable
+    {
+        set
+        {
+            _targetable = value;
+
+            //rb.simulated = value;
+            physicsCollider.enabled = value;
+        }
+        get
+        {
+            return _targetable;
+        }
+    }
+
+    public float _health = 3;
+    public bool _targetable = true;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+        //this is here to make sure isAlive boolean parameters is set to true when starting
+        animator.SetBool("isAlive", isAlive);
+
+        rb = GetComponent<Rigidbody2D>();
+        physicsCollider = GetComponent<Collider2D>();
+
+        //get player transform
+        playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        //check if player is attack from left or right and switch animation on x axis accordingly
+        CheckAttackDirection();
+    }
+
+
+    public void OnHit(float damage, Vector2 knockback)
+    {
+        Health -= damage;
+
+        //Apply force to slime enemy
+        rb.AddForce(knockback);
+    }
+
+    public void OnHit(float damage)
+    {
+        Health -= damage;
+    }
+
+    public void MakeUntargetable()
+    {
+        rb.simulated = false;
+    }
+
+    public void OnObjectDestroyed()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+    void CheckAttackDirection()
+    {
+        //determine if player direction is left or right
+        float direction = playerTransform.position.x < transform.position.x ? -1f : 1f;
+
+        //flip slime animation based on attack direction
+        if(direction < 0f)
+        {
+            //player on left
+            animator.transform.localScale = new Vector3(-1, 1, 1);
+        } else
+        {
+            //player on right
+            animator.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
 
 
     void OnCollisionEnter2D(Collision2D col)
--- Assets/Characters/Player/PlayerHealth.cs
+++ Assets/Characters/Player/PlayerHealth.cs
@@ -2,26 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour, IDamageable
+public class PlayerHealth : MonoBehaviour
 {
-    public float Health { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-    public bool Targetable { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-
-    public float _health = 3f;
-
-    public bool _targetable = true;
-    public void OnHit(float damage, Vector2 knockback)
+    // Start is called before the first frame update
+    void Start()
     {
-        throw new System.NotImplementedException();
+        
     }
 
-    public void OnHit(float damage)
+    // Update is called once per frame
+    void Update()
     {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnObjectDestroyed()
-    {
-        throw new System.NotImplementedException();
+        
     }
 }
