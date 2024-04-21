ae784e49fa4fe0d741db474a3113f663c5b33d06
Thomas P Klar <97933651+klarvo@users.noreply.github.com>
2024-04-21 19:30:11 -0400

    Added SwordSFX

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("------ Audio Source ------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("------ Audio Clip ------")]
+    public AudioClip background;
+    public AudioClip swordslash;
+    public AudioClip death;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
